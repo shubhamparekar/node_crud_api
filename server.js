b2a1118adbf75d64480cc0e9617948d63a557255
Shubham Parekar <shubham.parekar@qentelli.com>
2022-01-27 16:12:06 +0530

    Connection to DB established

--- server.js
+++ server.js
@@ -4,27 +4,35 @@ const express= require('express')
 
 //Body parser for getting the data through the urls
 const bodyParser=require('body-parser')
-const { request } = require('express')
 
 //Const app constrols the entire app with express functional constructor
+//Creating app function from the express funtional constructor to use it for creating server and APIs
 const app=express()
 
 
 // We are saying expressjs that to use body parser url encoded to be true
+//Enabling body parser with URLencoded from data to be true
 app.use(bodyParser.urlencoded({extended:true}))
+
+//Importing Mongo client
+const MongoClient = require('mongodb').MongoClient
+
+//Database connection string
+const connectionString = "mongodb+srv://shubham:shubham@cluster0.f1yma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+//Connection the database
+MongoClient.connect(connectionString, {useUnifiedTopology:true})
+    .then(client => {
+        console.log('connected to database')
+        const db = client.db('star-wars-quotes')
+})
 
 //Create route with creating the quotes
 app.post('/quotes',(req,res)=>{
-    console.log(request)
+    res.send(req.body)
 })
 
-
-
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + './index.html')
-})
-
-const PORT=3000
+const PORT=5000
 
 app.listen(PORT,()=>{
     console.log(`server running at port ${PORT}`)
